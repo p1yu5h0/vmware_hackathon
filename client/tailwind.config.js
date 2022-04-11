cb1354614242ebd6305887d5644a0ac9176861ae
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-04-11 10:39:56 +0530

    Addded configurations

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        bgcolor : '#011420',
+        primary : '#429FAD',
+        sgray : '#F0F0F0'
+      }
+    },
+  },
+  plugins: [],
+}
